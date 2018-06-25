a479ac6ca44fbee43ef153a72aba5e5c09910376
lienyo <suen2@lssh.tp.edu.tw>
2018-06-25 21:12:22 +0800

    first commit

--- if.py
+++ if.py
@@ -4,4 +4,4 @@ money=int(input("how much money"))
 if money<30000:
     print("little")
 else:    
-    print("a lotvvvvvvv of")
+    print("a lot of")
---
+++ start001.py
@@ -0,0 +1,15 @@
+# dfdfdfdzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+print("哈3.5")
+print(True)
+print(False)
+print(5//2)
+x=input("enter a num:")
+y=input("enter a num:")
+x=int(x)
+y=int(y)
+z=x+y
+print(x,y,z)
+x="vvvvvvvvvvcccvvvvvcccftvvvvv"
+print(x+"fff")
+a,b,c=input(a,b,c)
+print(a,b,c)
