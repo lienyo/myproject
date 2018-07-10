6214a1739720204e017f9089abf111ba4f2401f7
lienyo <40173815+lienyo@users.noreply.github.com>
2018-07-10 12:51:13 +0800

    Add files via upload

---
+++ sort5.py
@@ -0,0 +1,21 @@
+def add(*ns):
+    sum=0
+    for n in ns:
+        sum=sum+n
+    print(sum)
+
+l = []
+for i in range(5):
+    x = int(input('integer:\n'))
+    l.append(x)
+print('\n原始輸入=',sep=' ',end=' ')
+print (l)
+print('排序=',sep=' ',end=' ')
+l.sort()
+print (l)
+print('總和=',sep=' ',end=' ')
+add(*l)
+print('最大值=',sep=' ',end=' ')
+print(max(*l))
+print('最小值=',sep=' ',end=' ')
+print(min(*l))
