46f80e9fd3e0c67e1775ffdd7690f424c17f2924
lienyo <40173815+lienyo@users.noreply.github.com>
2018-07-10 15:51:35 +0800

    Update work2.py

--- work2.py
+++ work2.py
@@ -31,5 +31,5 @@ print('---------------------------------------------------------------')
 
 for x in range(1, 10):
     print
-    for y in range(1, x+1):
+    for y in range(1, 10):
         print ('%d*%d=%d' % (x, y, x*y))
